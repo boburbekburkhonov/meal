724cb5374b2a42d2f59cc01de760f142355b2e21
boburbekburkhonov <zafaroviccc@gmail.com>
2022-08-15 05:49:17 +0500

    feat: header hamburger

--- src/Component/Header/index.jsx
+++ src/Component/Header/index.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import logo from '../../assets/images/logo.png'
 import facebook from '../../assets/images/facebook.png'
 import twitter from '../../assets/images/twitter.png'
 import './main.css'
 
 function index(props) {
+
+  const[hamburger, setHamburger] = useState(false)
+  const nav = useRef()
+
+  function add(){
+    nav.current.classList.toggle('active');
+  }
+
   return (
     <header className='header'>
       <div className="container d-flex align-items-center justify-content-between">
         <a className='header-link-logo text-decoration-none' href="#">
-          <img src={logo} alt="logo" width='296' height='41' />
+          <img className='header-img' src={logo} alt="logo" width='296' height='41' />
         </a>
 
-        <nav className='sitenav d-flex align-items-center'>
+        <nav ref={nav} className='sitenav'>
           <ul className='list list-unstyled p-0 d-flex align-items-center'>
             <li>
               <a className='sitenav-link sitenav-link-active text-decoration-none' href="#">
@@ -37,6 +45,8 @@ function index(props) {
             <input type="search" placeholder='Search' className='sitenav-input' />
 
         </nav>
+
+        <button className='header-hamburger' onClick={() => add()}></button>
       </div>
     </header>
   );

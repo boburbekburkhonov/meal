8a072d3be3419dc06b72bbeb608708786cc141bf
boburbekburkhonov <zafaroviccc@gmail.com>
2022-08-16 05:53:33 +0500

    feat: hero

---
+++ src/Component/Hero/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import mealIcon from '../../assets/images/meal-icon.png'
+import './main.css'
+
+function index(props) {
+  return (
+    <div className='hero'>
+      <div className="container hero__container d-flex justify-content-between align-items-center">
+      <img src={mealIcon} alt="meal" width='142' height='142' />
+
+        <h1 className='hero-heading'>
+          Welcome to world foods
+        </h1>
+
+
+       <img src={mealIcon} alt="meal" width='142' height='142' />
+      </div>
+    </div>
+  );
+}
+
+export default index;
--- src/Component/Main/index.jsx
+++ src/Component/Main/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header'
+import Hero from '../Hero'
 import Footer from '../Footer'
 
 function index(props) {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
